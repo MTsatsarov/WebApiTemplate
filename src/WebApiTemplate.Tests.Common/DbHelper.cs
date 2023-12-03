a5c3dd148c2508df9c6a4b786864b9ff1065a45a
Mihail Tsatsarov <77450034+MTsatsarov@users.noreply.github.com>
2023-12-03 14:11:25 +0200

    Create tests projects

---
+++ src/WebApiTemplate.Tests.Common/DbHelper.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using WebApiTemplate.Data;
+
+namespace WebApiTemplate.Tests.Common
+{
+	[SetUpFixture]
+	public class DbHelper
+	{
+		private readonly string connectionString = 
+			"Server=.\\SQLEXPRESS;Database=WebApiTemplateTestContext;Trusted_Connection=True;MultipleActiveResultSets=true";
+
+		private static ApplicationDbContext db;
+
+		[OneTimeSetUp]
+		public void GlobalSetup()
+		{
+			db = CreateDbContext();
+		}
+
+		[OneTimeTearDown]
+		public void GlobalTeardown()
+		{
+			db.Database.EnsureDeleted();
+			db.Dispose();
+		}
+
+		private ApplicationDbContext CreateDbContext()
+		{
+			var dbContextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+				.UseSqlServer(connectionString)
+				.Options;
+
+			return new ApplicationDbContext(dbContextOptions);
+		}
+
+		public static ApplicationDbContext GetDbContext() => db;
+	}
+}
